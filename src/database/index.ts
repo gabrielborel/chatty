55b9ff96f80154bfcfb4d11e852edbe8cc777e4d
Gabriel Borel <biel_borel@hotmail.com>
2022-10-23 09:10:10 -0300

    Setting entitie created

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,10 @@
 import { DataSource } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { CreateSettings1666525963593 as CreateSettings } from "./migrations/1666525963593-CreateSettings";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
   migrations: [CreateSettings],
+  entities: [Setting],
 });
