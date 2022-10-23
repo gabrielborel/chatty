bd6fd31e72104305df3186847953fad451a2fff8
Gabriel Borel <biel_borel@hotmail.com>
2022-10-23 10:26:48 -0300

    User entity created

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "typeorm";
 import { Setting } from "../entities/Setting";
+import { User } from "../entities/User";
 import { CreateSettings1666525963593 as CreateSettings } from "./migrations/1666525963593-CreateSettings";
 import { CreateUsers1666531350355 as CreateUsers } from "./migrations/1666531350355-CreateUsers";
 
@@ -7,5 +8,5 @@ export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
   migrations: [CreateSettings, CreateUsers],
-  entities: [Setting],
+  entities: [Setting, User],
 });
