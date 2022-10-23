9f8738f8adde2de9fe6f592af1a821d5a0e58c97
Gabriel Borel <biel_borel@hotmail.com>
2022-10-23 14:35:17 -0300

    CreateMessages migration and Message entity created

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,14 @@
 import { DataSource } from "typeorm";
+import { Message } from "../entities/Message";
 import { Setting } from "../entities/Setting";
 import { User } from "../entities/User";
 import { CreateSettings1666525963593 as CreateSettings } from "./migrations/1666525963593-CreateSettings";
 import { CreateUsers1666531350355 as CreateUsers } from "./migrations/1666531350355-CreateUsers";
+import { CreateMessages1666533431464 as CreateMessages } from "./migrations/1666533431464-CreateMessages";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
-  migrations: [CreateSettings, CreateUsers],
-  entities: [Setting, User],
+  migrations: [CreateSettings, CreateUsers, CreateMessages],
+  entities: [Setting, User, Message],
 });
