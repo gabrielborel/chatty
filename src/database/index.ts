ed2734005006df82247f959c9931f73b67a2f5fb
Gabriel Borel <biel_borel@hotmail.com>
2022-10-23 10:24:53 -0300

    CreateUsers migration created

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm";
 import { Setting } from "../entities/Setting";
 import { CreateSettings1666525963593 as CreateSettings } from "./migrations/1666525963593-CreateSettings";
+import { CreateUsers1666531350355 as CreateUsers } from "./migrations/1666531350355-CreateUsers";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
-  migrations: [CreateSettings],
+  migrations: [CreateSettings, CreateUsers],
   entities: [Setting],
 });
