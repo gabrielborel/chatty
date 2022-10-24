cf9681ba918b5b93ae4b67fe422364c421955439
Gabriel Borel <biel_borel@hotmail.com>
2022-10-24 08:44:25 -0300

    CreateConnections migration and Connection entity created

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,21 @@
 import { DataSource } from "typeorm";
+import { Connection } from "../entities/Connection";
 import { Message } from "../entities/Message";
 import { Setting } from "../entities/Setting";
 import { User } from "../entities/User";
 import { CreateSettings1666525963593 as CreateSettings } from "./migrations/1666525963593-CreateSettings";
 import { CreateUsers1666531350355 as CreateUsers } from "./migrations/1666531350355-CreateUsers";
 import { CreateMessages1666533431464 as CreateMessages } from "./migrations/1666533431464-CreateMessages";
+import { CreateConnections1666565099946 as CreateConnections } from "./migrations/1666565099946-CreateConnections";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
-  migrations: [CreateSettings, CreateUsers, CreateMessages],
-  entities: [Setting, User, Message],
+  migrations: [CreateSettings, CreateUsers, CreateMessages, CreateConnections],
+  entities: [Setting, User, Message, Connection],
 });
+
+dataSource
+  .initialize()
+  .then(() => console.log("Data Source started."))
+  .catch((err) => console.log(err));
---
+++ src/entities/Connection.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { User } from "./User";
+
+@Entity("connections")
+export class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @JoinColumn({ name: "user_id" })
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  socket_id: string;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
